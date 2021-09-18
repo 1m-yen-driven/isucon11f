a547f884c576e80092f59fc201685f2c89d717bb
murata <murata@kmc.gr.jp>
2021-09-18 14:58:02 +0900

    not skip

--- deploy_golang.sh
+++ deploy_golang.sh
@@ -4,7 +4,7 @@ cd go
 make cross-compile
 
 for app in "$@"; do
-  # ssh -o ClearAllForwardings=yes -tt $app /home/isucon/logs/parse.sh skip
+  ssh -o ClearAllForwardings=yes $app -tt "bash logs/parse.sh skip"
   ssh -o ClearAllForwardings=yes $app sudo systemctl stop mysql isucholar.go.service nginx
   scp isucholar $app:/home/isucon/webapp/go/isucholar
   ssh -o ClearAllForwardings=yes $app sudo systemctl start mysql isucholar.go.service nginx
