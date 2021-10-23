051cc2dff56cfa98c6e9789321d6b71c33d0e87b
aokabi <aokabit@gmail.com>
2021-10-23 18:11:09 +0900

    userだけreplica

--- go/main.go
+++ go/main.go
@@ -184,7 +184,7 @@ func (h *handlers) Initialize(c echo.Context) error {
 	ctx := context.Background()
 	{
 		unreads := []UnreadAnnouncements{}
-		h.Replica.Select(&unreads, "SELECT * FROM `unread_announcements`")
+		h.DB.Select(&unreads, "SELECT * FROM `unread_announcements`")
 		rdb0.FlushDB(ctx)
 		pipe := rdb0.Pipeline()
 		defer pipe.Close()
@@ -481,7 +481,7 @@ func (h *handlers) GetMe(c echo.Context) error {
 	}
 
 	// var userCode string
-	// if err := h.Replica.Get(&userCode, "SELECT `code` FROM `users` WHERE `id` = ?", userID); err != nil {
+	// if err := h.DB.Get(&userCode, "SELECT `code` FROM `users` WHERE `id` = ?", userID); err != nil {
 	// 	c.Logger().Error(err)
 	// 	return c.NoContent(http.StatusInternalServerError)
 	// }
@@ -753,7 +753,7 @@ func (h *handlers) GetGrades(c echo.Context) error {
 		" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
 		" LEFT JOIN `classes` ON `classes`.`course_id` = `courses`.`id`" +
 		" WHERE `registrations`.`user_id` = ? ORDER BY `classes`.`part` DESC"
-	if err := h.Replica.Select(&registeredClasses, query, userID); err != nil {
+	if err := h.DB.Select(&registeredClasses, query, userID); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
@@ -772,14 +772,14 @@ func (h *handlers) GetGrades(c echo.Context) error {
 		var submissionsCount int
 		if classWithCourse.Class.ID != nil {
 			// TODO: N+1 (gnu)
-			if err := h.Replica.Get(&submissionsCount, "SELECT COUNT(*) FROM `submissions` WHERE `class_id` = ?", classWithCourse.Class.ID); err != nil {
+			if err := h.DB.Get(&submissionsCount, "SELECT COUNT(*) FROM `submissions` WHERE `class_id` = ?", classWithCourse.Class.ID); err != nil {
 				c.Logger().Error(err)
 				return c.NoContent(http.StatusInternalServerError)
 			}
 
 			var myScore sql.NullInt64
 			// TODO: N+1 (gnu)
-			if err := h.Replica.Get(&myScore, "SELECT `submissions`.`score` FROM `submissions` WHERE `user_id` = ? AND `class_id` = ?", userID, classWithCourse.Class.ID); err != nil && err != sql.ErrNoRows {
+			if err := h.DB.Get(&myScore, "SELECT `submissions`.`score` FROM `submissions` WHERE `user_id` = ? AND `class_id` = ?", userID, classWithCourse.Class.ID); err != nil && err != sql.ErrNoRows {
 				c.Logger().Error(err)
 				return c.NoContent(http.StatusInternalServerError)
 			} else if err == sql.ErrNoRows || !myScore.Valid {
@@ -818,7 +818,7 @@ func (h *handlers) GetGrades(c echo.Context) error {
 			" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
 			" WHERE `courses`.`id` = ?" +
 			" GROUP BY `users`.`id`"
-		if err := h.Replica.Select(&totals, query, res.Course.ID); err != nil {
+		if err := h.DB.Select(&totals, query, res.Course.ID); err != nil {
 			c.Logger().Error(err)
 			return c.NoContent(http.StatusInternalServerError)
 		}
@@ -896,7 +896,7 @@ func getGpa(h *handlers, c echo.Context) ([]float64, error) {
 		" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
 		" WHERE `users`.`type` = ?" +
 		" GROUP BY `users`.`id`"
-	if err := h.Replica.Select(&gpas, query, StatusClosed, StatusClosed, Student); err != nil {
+	if err := h.DB.Select(&gpas, query, StatusClosed, StatusClosed, Student); err != nil {
 		c.Logger().Error(err)
 		return nil, fmt.Errorf("hoge %w", err)
 	}
@@ -981,7 +981,7 @@ func (h *handlers) SearchCourses(c echo.Context) error {
 
 	// 結果が0件の時は空配列を返却
 	res := make([]GetCourseDetailResponse, 0)
-	if err := h.Replica.Select(&res, query+condition, args...); err != nil {
+	if err := h.DB.Select(&res, query+condition, args...); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
@@ -1056,7 +1056,7 @@ func (h *handlers) AddCourse(c echo.Context) error {
 	if err != nil {
 		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
 			var course Course
-			if err := h.Replica.Get(&course, "SELECT * FROM `courses` WHERE `code` = ?", req.Code); err != nil {
+			if err := h.DB.Get(&course, "SELECT * FROM `courses` WHERE `code` = ?", req.Code); err != nil {
 				c.Logger().Error(err)
 				return c.NoContent(http.StatusInternalServerError)
 			}
@@ -1098,7 +1098,7 @@ func (h *handlers) GetCourseDetail(c echo.Context) error {
 		" FROM `courses`" +
 		" JOIN `users` ON `courses`.`teacher_id` = `users`.`id`" +
 		" WHERE `courses`.`id` = ?"
-	if err := h.Replica.Get(&res, query, courseID); err != nil && err != sql.ErrNoRows {
+	if err := h.DB.Get(&res, query, courseID); err != nil && err != sql.ErrNoRows {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	} else if err == sql.ErrNoRows {
@@ -1171,7 +1171,7 @@ func (h *handlers) GetClasses(c echo.Context) error {
 	courseID := c.Param("courseID")
 	// courses は増えることはあっても消えることはないのでTransactionは不要
 	var count int
-	if err := h.Replica.Get(&count, "SELECT COUNT(*) FROM `courses` WHERE `id` = ?", courseID); err != nil {
+	if err := h.DB.Get(&count, "SELECT COUNT(*) FROM `courses` WHERE `id` = ?", courseID); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
@@ -1249,7 +1249,7 @@ func (h *handlers) AddClass(c echo.Context) error {
 		_ = tx.Rollback()
 		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
 			var class Class
-			if err := h.Replica.Get(&class, "SELECT * FROM `classes` WHERE `course_id` = ? AND `part` = ?", courseID, req.Part); err != nil {
+			if err := h.DB.Get(&class, "SELECT * FROM `classes` WHERE `course_id` = ? AND `part` = ?", courseID, req.Part); err != nil {
 				c.Logger().Error(err)
 				return c.NoContent(http.StatusInternalServerError)
 			}
@@ -1415,7 +1415,7 @@ func (h *handlers) RegisterScores(c echo.Context) error {
 	classID := c.Param("classID")
 
 	var submissionClosed bool
-	if err := h.Replica.Get(&submissionClosed, "SELECT `submission_closed` FROM `classes` WHERE `id` = ? FOR SHARE", classID); err != nil && err != sql.ErrNoRows {
+	if err := h.DB.Get(&submissionClosed, "SELECT `submission_closed` FROM `classes` WHERE `id` = ? FOR SHARE", classID); err != nil && err != sql.ErrNoRows {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	} else if err == sql.ErrNoRows {
@@ -1708,7 +1708,7 @@ func (h *handlers) AddAnnouncement(c echo.Context) error {
 		_ = tx.Rollback()
 		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
 			var announcement Announcement
-			if err := h.Replica.Get(&announcement, "SELECT * FROM `announcements` WHERE `id` = ?", req.ID); err != nil {
+			if err := h.DB.Get(&announcement, "SELECT * FROM `announcements` WHERE `id` = ?", req.ID); err != nil {
 				c.Logger().Error(err)
 				return c.NoContent(http.StatusInternalServerError)
 			}
